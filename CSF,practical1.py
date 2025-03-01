f72ae7297f11fe1a96779d52d83398c5945615a4
Abeshek Limbu <02240111.cst@rub.edu.bt>
2025-03-02 00:36:56 +0600

    Man

--- CSF,practical1.py
+++
@@ -1,4 +0,0 @@
-studentname = "Abeshek"
-studentnumber = "0224011"
-
-print(studentnumber + studentname + " im  the hottest man in my class and i love my samu" )
---
+++ CSF_practical0 .py
@@ -0,0 +1,4 @@
+studentname = "Abeshek"
+studentnumber = "0224011"
+
+print(studentnumber + studentname + " An honest man" )
