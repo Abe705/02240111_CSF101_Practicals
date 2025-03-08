f7657335ec27d299563a0447ab09543bd125d85a
Abeshek Limbu <02240111.cst@rub.edu.bt>
2025-03-08 22:03:52 +0600

    my final commit

---
+++ Class Learning.py
@@ -0,0 +1,11 @@
+newArray = ["football",12,True,0.3,None]
+
+print(len(newArray))
+newArray.append("Mobile Lengend")
+
+firstArray = newArray
+print(firstArray)
+
+newArray.append("Subject")
+secondArray = firstArray 
+print(secondArray)
---
+++ practical in class.py
@@ -0,0 +1,13 @@
+#Exercise
+
+samplearray = ["ECE","WRE","IT","AE","ICE","ME","EC","CE","GE","SE"]
+arraylem = len(samplearray)
+newarray=[]
+for index in range(arraylen):
+    allelement = samplearray[index]
+    newarray.append((allelement.lower))
+
+for secondIndex in range(len(newarray)):
+    print(newarray[secondIndex])
+
+    
