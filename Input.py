e6c347ccb626983e35a413a255308853e39366b9
Abeshek Limbu <02240111.cst@rub.edu.bt>
2025-03-17 16:35:36 +0600

    my final class work

---
+++ Input.py
@@ -0,0 +1,24 @@
+#Write a program to input 2 number and print their sum
+
+number1 = int(input("Enter your First Number:"))
+number2 = int(input("Enter Your Second Number:"))
+print("sum =", number1 + number2)
+
+#Write a program to input side of a square and print its area
+
+side = int(input("Enter your number:"))
+
+print("area=", side * side)
+
+# write a program to input 2 floating point numbers and print their values
+
+a = float(input("enter your number:"))
+b = float(input("enter your second number:"))
+print("avg=", (a + b)/2)
+
+# WAP to input 2 int numbers, c and d. Print Ture if c is greater tahn or equal to d. False if c is greater
+
+c = int(input("enter first:"))
+d = int(input("enter second:"))
+
+print(c>=d)
